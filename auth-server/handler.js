f40ca69873b6b53975abcd2cd0e1c37465a40cce
priegels <88594441+priegels@users.noreply.github.com>
2021-12-08 12:30:40 +0100

    CORS allowance

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -89,6 +89,9 @@ module.exports.getAccessToken = async (event) => {
       // Respond with OAuth token
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
         body: JSON.stringify(token),
       };
     })
