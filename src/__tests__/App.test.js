6dd2da36977d6ec14eaf68a8824597fcb826e793
priegels <88594441+priegels@users.noreply.github.com>
2021-12-11 12:44:26 +0100

    testing for Feature 1 Scenario 2 & 3

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
 
+  // refactoring; beforeAll to not rewrite this function in every test
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
 });
