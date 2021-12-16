bb3e7a937da4a059f99dfbcd20b7e100cc15a51a
priegels <88594441+priegels@users.noreply.github.com>
2021-12-16 12:09:48 +0100

    Added some info to be displayed by event

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    const { event } = this.props;
+
+    return <div className="event">
+      <h2 className="summary">{event.summary}</h2>
+
+      <p className="start-date">
+        {event.start.dateTime} ({event.start.timeZone})
+      </p>
+
+      <p className="location">
+        @{event.summary} | {event.location}
+      </p>
+
+      <button className="show-details-button">Show Details</button> 
+    </div>;
   }
 }
 
