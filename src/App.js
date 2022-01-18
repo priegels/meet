85f1d5d5fe00882dfd4d2b9096f2ff917ce2ff64
priegels <88594441+priegels@users.noreply.github.com>
2022-01-18 21:27:27 +0100

    revamped componentDidMount

--- src/App.js
+++ src/App.js
@@ -43,13 +43,20 @@ class App extends Component {
     this.updateEvents(currentLocation, e);    
   }  
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   componentWillUnmount(){
