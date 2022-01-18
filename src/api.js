c62e073625cf14b6ade1e04a3b6b9c762aa6369c
priegels <88594441+priegels@users.noreply.github.com>
2022-01-18 21:26:24 +0100

    exported checkToken function

--- src/api.js
+++ src/api.js
@@ -18,7 +18,7 @@ export const extractLocations = (events) => {
 };
 
 //access token found in storage
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
