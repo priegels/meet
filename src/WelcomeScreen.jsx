31868599a023abcb68da48cebb8f7f71fe8688ac
priegels <88594441+priegels@users.noreply.github.com>
2022-01-19 14:57:10 +0100

    styling of Welcome Screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import './WelcomeScreen.css';
 
+import logo from './img/logo/logo.svg';
+
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
   (
     <div className="WelcomeScreen">
-      <h1>Welcome to the Meet app</h1>
+      <h1>Welcome to</h1>
+      <img src={logo} alt="meet app logo" height={200} width={200} />
+      <div className="welcome-spacer"></div>
       <h4>
         Log in to see upcoming events around the world for full-stack developers
       </h4>
         <div className="button_cont" align="center">
           <div class="google-btn">
             <div class="google-icon-wrapper">
-              <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G22_Logo.svg" 
+              <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
               alt="Google sign-in" />
             </div>
             <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener" class="btn-text">
-              <b>Sign in with google</b>
+              Sign in with google
             </button>
           </div>
         </div> 
